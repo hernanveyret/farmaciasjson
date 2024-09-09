dbd1f005353a398b226855409779780556bcb91a
hernanveyret <hernanveyret@hotmail.com>
2024-09-09 18:19:10 -0300

    2024 completo

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 const fs = require('fs');
-const data = require('./pharmacies.json')
-const dataDb = require('./pharmaciesDb.json')
-dataDb[2024][1][10].forEach(e=> {
+const data = require('./farmacias.json')
+const dataDb = require('./farmacias2024.json')
+
+
+dataDb[2024][0][1].forEach(e=> {
   if(e.dateShift.includes(e.dateShift)){
      e.pharmacies.push(data[e.dateShift])
    }    
        })
-       dataDb[2024][2][11].forEach(e=> {
+
+       dataDb[2024][1][2].forEach(e=> {
       if(e.dateShift.includes(e.dateShift)){
       e.pharmacies.push(data[e.dateShift])
       }     
   })
-  dataDb[2024][3][12].forEach(e=> {
+
+  dataDb[2024][2][3].forEach(e=> {
       if(e.dateShift.includes(e.dateShift)){
       e.pharmacies.push(data[e.dateShift])
       }    
-  })     
-     console.log(dataDb[2024][0][9][0].pharmacies)
-     console.log(dataDb[2024][1][10][0].pharmacies)
-     console.log(dataDb[2024][2][11][0].pharmacies)
-     console.log(dataDb[2024][3][12][0].pharmacies)
+  })
+
+  dataDb[2024][3][4].forEach(e=> {
+    if(e.dateShift.includes(e.dateShift)){
+    e.pharmacies.push(data[e.dateShift])
+    }    
+})
+
+dataDb[2024][4][5].forEach(e=> {
+    if(e.dateShift.includes(e.dateShift)){
+    e.pharmacies.push(data[e.dateShift])
+    }    
+})
+
+dataDb[2024][5][6].forEach(e=> {
+    if(e.dateShift.includes(e.dateShift)){
+    e.pharmacies.push(data[e.dateShift])
+    }    
+})
+
+dataDb[2024][6][7].forEach(e=> {
+    if(e.dateShift.includes(e.dateShift)){
+    e.pharmacies.push(data[e.dateShift])
+    }    
+})
+
+dataDb[2024][7][8].forEach(e=> {
+    if(e.dateShift.includes(e.dateShift)){
+    e.pharmacies.push(data[e.dateShift])
+    }    
+})
 
 fs.writeFile('algo.json',JSON.stringify(dataDb, null, 2) ,(err) => {
     if(err){
