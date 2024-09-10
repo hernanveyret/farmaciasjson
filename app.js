409e39c95e1658d48b13753bcde3af4932336558
hernanveyret <hernanveyret@hotmail.com>
2024-09-10 13:56:55 -0300

    aaaa

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
 const data = require('./farmacias.json')
-const dataDb = require('./farmacias2024.json')
+const dataDb = require('./datafarmacias2024.json')
+
+console.log(dataDb[2024][0][1][1])
+
 
 
 dataDb[2024][0][1].forEach(e=> {
+
+
+ console.log(data[e.dateShift])
+ 
   if(e.dateShift.includes(e.dateShift)){
+
+    
      e.pharmacies.push(data[e.dateShift])
    }    
        })
 
-       dataDb[2024][1][2].forEach(e=> {
-      if(e.dateShift.includes(e.dateShift)){
-      e.pharmacies.push(data[e.dateShift])
-      }     
-  })
-
+     
+/*
   dataDb[2024][2][3].forEach(e=> {
       if(e.dateShift.includes(e.dateShift)){
       e.pharmacies.push(data[e.dateShift])
@@ -50,6 +55,8 @@ dataDb[2024][7][8].forEach(e=> {
     e.pharmacies.push(data[e.dateShift])
     }    
 })
+
+*/
 
 fs.writeFile('algo.json',JSON.stringify(dataDb, null, 2) ,(err) => {
     if(err){
